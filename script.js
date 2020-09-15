6955c0d07b036fe3d52521dc119b064d25e89797
Yah-Yah <janeczka@gmail.com>
2020-09-15 16:44:18 +0100

    Add basic functions

--- script.js
+++ script.js
@@ -5,3 +5,28 @@
 4. Function to change todo item
 5. Function to delete todo item
 */
+// Create an array for todo items
+const todo = [];
+
+// Function to display todo
+function displayTodos() {
+  console.log('My todo: ' + todo);
+}
+
+// Function to add todo item
+function addTodo(newItem) {
+  todo.push(newItem);
+  displayTodos();
+}
+
+// Function to change todo item
+function changeTodo(index, newValue) {
+  todo[index] = newValue;
+  displayTodos();
+}
+
+// Function to delete todo item
+function deleteTodo(index) {
+  todo.splice(index, 1);
+  displayTodos();
+}
